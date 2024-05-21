c5e59aa6136bd28b178c96543108eebe3f3f96d1
Valoulec <valentin.leclerc78@gmail.com>
2024-05-21 11:54:21 +0200

    fichier créer

---
+++ src/app/artisant/artisant.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artisant',
+  standalone: true,
+  imports: [],
+  templateUrl: './artisant.component.html',
+  styleUrl: './artisant.component.css'
+})
+export class ArtisantComponent {
+
+}
---
+++ src/app/fiche/fiche.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fiche',
+  standalone: true,
+  imports: [],
+  templateUrl: './fiche.component.html',
+  styleUrl: './fiche.component.css'
+})
+export class FicheComponent {
+
+}
